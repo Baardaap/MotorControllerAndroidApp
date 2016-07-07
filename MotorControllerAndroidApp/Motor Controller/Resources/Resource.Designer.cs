980dc2afa1932e030f88e8540498203859701acd
Ian <ian_seelen@hotmail.com>
2016-07-07 16:30:22 +0200

    Added basic GUI

--- MotorControllerAndroidApp/Motor Controller/Resources/Resource.Designer.cs
+++ MotorControllerAndroidApp/Motor Controller/Resources/Resource.Designer.cs
@@ -57,6 +57,31 @@ namespace Motor_Controller
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int seekMotor1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int seekMotor2 = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int textMotor1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textMotor2 = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
