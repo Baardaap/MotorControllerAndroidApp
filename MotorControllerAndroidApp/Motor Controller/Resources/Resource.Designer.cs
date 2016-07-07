1a08170e60b03e484a52ab3455adbd11fdc54bd6
Ian <ian_seelen@hotmail.com>
2016-07-07 16:55:09 +0200

    GUI first 3 motors complete

--- MotorControllerAndroidApp/Motor Controller/Resources/Resource.Designer.cs
+++ MotorControllerAndroidApp/Motor Controller/Resources/Resource.Designer.cs
@@ -63,14 +63,29 @@ namespace Motor_Controller
 			// aapt resource value: 0x7f050001
 			public const int seekMotor1 = 2131034113;
 			
-			// aapt resource value: 0x7f050003
-			public const int seekMotor2 = 2131034115;
+			// aapt resource value: 0x7f050004
+			public const int seekMotor2 = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int seekMotor3 = 2131034119;
 			
 			// aapt resource value: 0x7f050000
 			public const int textMotor1 = 2131034112;
 			
+			// aapt resource value: 0x7f050003
+			public const int textMotor2 = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int textMotor3 = 2131034118;
+			
 			// aapt resource value: 0x7f050002
-			public const int textMotor2 = 2131034114;
+			public const int textPersentMotor1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int textPersentMotor2 = 2131034117;
+			
+			// aapt resource value: 0x7f050008
+			public const int textPersentMotor3 = 2131034120;
 			
 			static Id()
 			{
